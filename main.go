356f438acdf488ac823933438f3ff004d8cc5888
mlaane <goobric@gmail.com>
2024-05-24 18:43:34 +0100

    minor update for bckend port

--- main.go
+++ main.go
@@ -13,9 +13,9 @@ func main() {
     // Define a route for the GET method on the root path '/'
     app.Get("/", func(c fiber.Ctx) error {
         // Send a string response to the client
-        return c.SendString("Hello, World 👋!")
+        return c.SendString("Hello, World!")
     })
 
-    // Start the server on port 3000
-    log.Fatal(app.Listen(":3000"))
+    // Start the server on port 8080
+    log.Fatal(app.Listen(":8080"))
 }
